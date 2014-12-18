ba1f55b7f5817eebd704f3b7b8dc357e78317785
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-12-18 14:43:51 +0000

    started with bindings for devPCIE
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@705 5f601a5c-88b9-47f3-b851-818f6741d00d

--- MtcaMappedDevicePython.cc
+++ MtcaMappedDevicePython.cc
@@ -1,5 +1,7 @@
 #include <boost/python.hpp>
+#include <MtcaMappedDevice/devPCIE.h>
 
-BOOST_PYTHON_MODULE(MtcaMappedDeviceModule){
-
+BOOST_PYTHON_MODULE(mtcamappeddevice) {
+  boost::python::class_<mtca4u::devPCIE, boost::noncopyable>("devPCIE")
+      .def("openDev", &mtca4u::devPCIE::openDev);
 }
