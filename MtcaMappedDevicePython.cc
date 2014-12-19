ad4e904b43847746310b0ea96ac1bf4ca8693abd
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-12-19 16:07:49 +0000

    typed in the api's for devPCIE class; things need work as api uses pass by reference to get and set register values. Have to figure out how this can be done
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@706 5f601a5c-88b9-47f3-b851-818f6741d00d

--- MtcaMappedDevicePython.cc
+++ MtcaMappedDevicePython.cc
@@ -1,7 +1,20 @@
 #include <boost/python.hpp>
 #include <MtcaMappedDevice/devPCIE.h>
 
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(openDevDefaultArgs,
+                                       mtca4u::devPCIE::openDev, 1, 3)
+
 BOOST_PYTHON_MODULE(mtcamappeddevice) {
+  boost::python::class_<mtca4u::devConfigBase>("devConfigBase");
+
   boost::python::class_<mtca4u::devPCIE, boost::noncopyable>("devPCIE")
-      .def("openDev", &mtca4u::devPCIE::openDev);
+      .def("openDev", &mtca4u::devPCIE::openDev, openDevDefaultArgs())
+      .def("closeDev", &mtca4u::devPCIE::closeDev)
+      .def("readReg", &mtca4u::devPCIE::readReg)
+      .def("writeReg", &mtca4u::devPCIE::writeReg)
+      .def("readArea", &mtca4u::devPCIE::readArea)
+      .def("writeArea", &mtca4u::devPCIE::writeArea)
+      .def("readDMA", &mtca4u::devPCIE::readDMA)
+      .def("writeDMA", &mtca4u::devPCIE::writeDMA)
+      .def("readDeviceInfo", &mtca4u::devPCIE::readDeviceInfo);
 }
