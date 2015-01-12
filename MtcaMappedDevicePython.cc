59437829c5811ea7530fe121c68d218497f03d90
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-01-12 14:22:20 +0000

    Fixed intendation
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@716 5f601a5c-88b9-47f3-b851-818f6741d00d

--- MtcaMappedDevicePython.cc
+++ MtcaMappedDevicePython.cc
@@ -1,7 +1,8 @@
 #include <MtcaMappedDevice/devPCIE.h>
 #include "WrapperMethods.h"
 
-namespace bp = boost::python;;
+namespace bp = boost::python;
+;
 
 // This covers the version of openDev method with default arguments
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(openDevDefaultArgs,
@@ -10,16 +11,15 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(openDevDefaultArgs,
 BOOST_PYTHON_MODULE(mtcamappeddevice) {
   bp::class_<mtca4u::devConfigBase>("devConfigBase");
 
-  bp::class_<mtca4u::devPCIE, boost::noncopyable/*copy constructor not defined*/>("devPCIE")
+  bp::class_<mtca4u::devPCIE,
+             boost::noncopyable /*copy constructor not defined*/>("devPCIE")
       .def("openDev", &mtca4u::devPCIE::openDev, openDevDefaultArgs())
       .def("closeDev", &mtca4u::devPCIE::closeDev)
-      .def("readReg", &readReg) // TODO: assign a proper namespace for code in WrapperMethods.cc
+      .def("readReg", &readReg) // TODO: assign a proper namespace for code in
+                                // WrapperMethods.cc
       .def("writeReg", &mtca4u::devPCIE::writeReg)
       .def("readDMA", &readDMA)
       .def("readArea", &readArea)
       .def("writeArea", &writeArea)
       .def("readDeviceInfo", &readDeviceInfo);
 }
-
-
-
