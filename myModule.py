4efb4686f6fdf9bc75ef140bf906d387b47434f4
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-03-16 11:57:28 +0000

    Filled in documentation for the interface methods - pending DMA related access methods
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@846 5f601a5c-88b9-47f3-b851-818f6741d00d

--- myModule.py
+++ myModule.py
@@ -41,20 +41,20 @@ class Device():
     """ Reads out Fixed point converted values from the opened mapped device
     
     This method uses the register mapping information to return Fixed Point
-    converted version of the values contained in a register. This method can be
+    converted versions of the values contained in a register. This method can be
     used to read in the whole register or an arbitary number of register
-    elements from anywhere in the span of the register (through the
+    elements from anywhere within the register (through the
     'offsetFromRegisterBaseAddress' parameter).
     
     Parameters
     ----------
     registerName : str
-      The name of the register on the device to which read access is sought.
+      The name of the register (on the device) to which read access is sought.
       
     numberOfElementsToRead : int, optional 
       Optional parameter specifying the number of register elements that should
-      be read out. The width of each register element is internally obtained
-      from the mapping file.
+      be read out. The width and fixed point representation of the register
+      element are internally obtained from the mapping file.
       
       The method returns all elements in the register if this parameter is
       ommitted or when its value is set as 0.
@@ -62,55 +62,215 @@ class Device():
     offsetFromRegisterBaseAddress : int, optional
       This is an offset from the start of the specified register's base address.
       An offset of 1 represents 32 bits. When an offset is provided as a
-      parameter, the method returns elements from this point in memory (/address
-      offset) onwards.
+      parameter, the method reads out elements from this point in memory
+      (/address offset) onwards.
       
     Returns
     -------
-    numpy.ndarray
-      The return type for the method is a 1-Dimensional array with datatype
-      numpy.float32. The returned 1-Dimensional numpy.ndarray would either
-      contain all elements in the register or only the number specified by the
+    numpy.array, dtype == numpy.float32
+      The return type for the method is a 1-Dimensional numpy array with
+      datatype numpy.float32. The returned numpy.array would either contain all
+      elements in the register or only the number specified by the
       numberOfElementsToRead parameter
      
+    Raises
+    ------
+    
     Examples
     --------
+    In the examples  register "WORD_CLK_MUX" is 4 elements long.
+    
     >>> device = Device("/dev/llrfdummys4","mapfiles/mtcadummy.map")
     
     >>> device.read("WORD_CLK_MUX")
-    array([15.0,14.0, 13.0, 12.0], dtype = float32)#TODO fill in the output
+    array([15.0, 14.0, 13.0, 12.0], dtype = float32)#TODO fill in the output
     
     >>> device.read("WORD_CLK_MUX", 0)
-    array([15.0,14.0, 13.0, 12.0], dtype = float32)#TODO fill in the output
+    array([15.0, 14.0, 13.0, 12.0], dtype = float32)#TODO fill in the output
         
-    >>> device.read("WORD_CLK_CNT", 1)
+    >>> device.read("WORD_CLK_MUX", 1)
     array([15.0], dtype = float32)#TODO fill in the output
     
-    >>> device.read("WORD_CLK_CNT", 1, 2 )
+    >>> device.read("WORD_CLK_MUX", 1, 2 )
     array([13.0], dtype = float32)
 
-    >>> device.read("WORD_CLK_CNT", 0, 2 )
+    >>> device.read("WORD_CLK_MUX", 0, 2 )
     array([13.0, 12.0], dtype = float32)
         
-    >>> device.read("WORD_CLK_CNT", numberOfElementsToRead=1, offsetFromRegisterBaseAddress=2 )
+    >>> device.read("WORD_CLK_MUX", numberOfElementsToRead=1, offsetFromRegisterBaseAddress=2 )
     array([13.0], dtype = float32)
     
-    >>> device.read("WORD_CLK_CNT", offsetFromRegisterBaseAddress=2 )
+    >>> device.read("WORD_CLK_MUX", offsetFromRegisterBaseAddress=2 )
     array([13.0, 12.0], dtype = float32)
+    
+    See Also
+    --------
+    Device.readRaw : Read in 'raw' bit values from a device register 
+
     """
     return numpy.array([0], dtype = numpy.float32)
-
-  def readRaw(self, registerName, numberOf32BitWordsToRead=1, 
-              offsetFromRegisterBaseAddress=0):
-    pass
   
   def write(self, registerName, dataToWrite, offsetFromRegisterBaseAddress=0):
-    pass
+    """ Sets data into a desired register
+    
+    This method writes values into a register on the board. The method
+    internally uses a fixed point converter that is aware of the register width
+    on the device and its fractional representation. This Fixed point converter
+    converts the double input into corresponding Fixed Point representaions that
+    fit into the decive register.
+    
+    Parameters
+    ----------
+    registerName : str
+      Mapped name of the register to which write access is sought
+      
+    dataToWrite : numpy.array(dtype = numpy.float32)  
+    A numpy array holding the  the values to be written in to the register. The
+    array is expected to hold numpy.float32 values. Each value in this array
+    represents an induvidual element of the register
+       
+    offsetFromRegisterBaseAddress : int, optional
+      This is an offset from the start of the specified register's base address.
+      An offset of 1 represents 32 bits. When an offset is provided as a
+      parameter, the method starts to write elements from this address offset
+      within the register.
+    
+    Returns
+    -------
+    None
+    
+    Raises
+    ------
+    datatype #TODO
+    
+    Examples
+    --------
+    In the examples, register "WORD_CLK_MUX" is 4 elements long.
+    >>> device = Device("/dev/llrfdummys4","mapfiles/mtcadummy.map")
+    
+    >>> dataToWrite = numpy.array([15.0, 14.0, 13.0, 12.0], dtype = float32)
+    >>> device.write("WORD_CLK_MUX", dataToWrite)
+    
+    >>> dataToWrite = numpy.array([13.0, 12.0], dtype = float32)
+    >>> device.write("WORD_CLK_MUX", dataToWrite, 2)
+    
+    """
+    return None
+  
+  def readRaw(self, registerName, numberOfElementsToRead=1, 
+              offsetFromRegisterBaseAddress=0):
+    """ Returns the raw values from a device's register
+    
+    This method returns the raw bit values contained in the queried register.
+    The returned values are not Fixed Point converted, but direct binary values
+    contained in the register elements.
+    
+    Parameters
+    ----------
+    registerName : str
+      The name of the device register to read from
+      
+     numberOfElementsToRead : int, optional
+      Optional parameter specifying the number of register elements that should
+      be read out.
+      
+      The method returns all elements in the register if this parameter is
+      ommitted or when its value is set as 0.
+    
+     offsetFromRegisterBaseAddress : int, optional
+      This is an offset from the start of the specified register's base address.
+      An offset of 1 represents 32 bits. When an offset is provided as a
+      parameter, the method reads out elements from this point in memory
+      (/address offset) onwards.
+    
+    Returns
+    -------
+    numpy.array, dtype == numpy.int32
+      The method returns a numpy.int32 array containing the raw bit values of
+      the register elements. The length of the array either equals the number of
+      elements that make up the register or the number specified through the
+      numberOfElementsToRead parameter
+    
+    Raises
+    ------
+    
+    Examples
+    --------
+    In the example, register "WORD_CLK_MUX" is 4 elements long.
+    >>> device = Device("/dev/llrfdummys4","mapfiles/mtcadummy.map")
+    
+    >>> device.read("WORD_CLK_MUX")
+    array([15, 14, 13, 12], dtype = int32)#TODO fill in the output
+    
+    >>> device.read("WORD_CLK_MUX", 0)
+    array([15, 14, 13, 12], dtype = int32)#TODO fill in the output
+        
+    >>> device.read("WORD_CLK_MUX", 1)
+    array([15], dtype = int32)#TODO fill in the output
+    
+    >>> device.read("WORD_CLK_MUX", 1, 2 )
+    array([13], dtype = int32)
 
+    >>> device.read("WORD_CLK_MUX", 0, 2 )
+    array([13, 12], dtype = int32)
+        
+    >>> device.read("WORD_CLK_MUX", numberOfElementsToRead=1, offsetFromRegisterBaseAddress=2 )
+    array([13], dtype = int32)
+    
+    >>> device.read("WORD_CLK_MUX", offsetFromRegisterBaseAddress=2 )
+    array([13, 12], dtype = int32)
+    
+    See Also
+    --------
+    Device.read : Read in Fixed Point converted bit values from a device
+    register
+
+    """
+    return numpy.array([0], dtype = numpy.int32)
+  
   def writeRaw(self, registerName, dataToWrite,
       offsetFromRegisterBaseAddress=0):
-    pass
+    """ Write raw bit values into the register
+    
+    Provides a way to put in a desired bit value into individual register
+    elements. 
+    
+    Parameters
+    ----------
+    registerName : str
+      The name of the desired register to write into.
+      
+    dataToWrite : numpy.array, dtype == numpy.int32
+     The array holding the bit values to be written into the register. The numpy
+     array is expected to contain numpy.int32 values
+     
+    offsetFromRegisterBaseAddress : int, optional
+      This is an offset from the start of the specified register's base address.
+      An offset of 1 represents 32 bits. When an offset is provided as a
+      parameter, the method starts to write elements from this address offset
+      within the register.  
+    
+    Returns
+    -------
+    None
+    
+    Raises
+    ------
+    datatype #TODO
+    
+    Examples
+    --------
+    In the examples, register "WORD_CLK_MUX" is 4 elements long.
+    >>> device = Device("/dev/llrfdummys4","mapfiles/mtcadummy.map")
+    
+    >>> dataToWrite = numpy.array([15, 14, 13, 12], dtype = float32)
+    >>> device.write("WORD_CLK_MUX", dataToWrite)
+    
+    >>> dataToWrite = numpy.array([13, 12], dtype = float32)
+    >>> device.write("WORD_CLK_MUX", dataToWrite, 2)
+        
+    
+    
+    """
+    return None
   
-  def wrapper(self, regoffset=0, numpyArray=numpy.array([1], dtype=numpy.int32),
-               size=1, bar=0):
-    pass
