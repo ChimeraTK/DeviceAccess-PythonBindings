ff2f815e8a5444dca8bca55da774fd413495704c
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-06-01 09:14:16 +0000

    - Introduced the effc++ flag for the project: - Used #pragma GCC diagnostic ignored "-Weffc++" in PythonDeviceWrapper.h to get   around -Weffc++ warning for boost::python::wrapper<mtca4upy::PythonDevice>   (non virtual destructor for a class tht uses virtual methods). The work around   should turn off -Weffc++ only for the code enclosed int #pragma blocks. See   code comments for more details
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@1049 5f601a5c-88b9-47f3-b851-818f6741d00d

--- PythonDeviceWrapper.h
+++ PythonDeviceWrapper.h
@@ -6,15 +6,26 @@
 #include <MtcaMappedDevice/devConfigBase.h>
 #include "PythonDevice.h"
 
+
+
+
 namespace mtca4upy { // TODO: Refactor to a better name
 
+//http://dbp-consulting.com/tutorials/SuppressingGCCWarnings.html
+// should temporarily disable the -Weffc++ flag
+// needed because boost::python::wrapper<mtca4upy::PythonDevice> throws a warning
+// for non virtual destructor with -Weffc++
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
 class PythonDeviceWrapper
     : public mtca4upy::PythonDevice,
       public boost::python::wrapper<mtca4upy::PythonDevice> {
+#pragma GCC diagnostic pop
+
 public:
   PythonDeviceWrapper() {};
 
-  inline void writeRaw(uint32_t regOffset, bp::numeric::array dataToWite,
+  virtual inline void writeRaw(uint32_t regOffset, bp::numeric::array dataToWite,
                        size_t bytesToWrite, uint8_t bar) {
     this->get_override("writeArea")(regOffset, dataToWite, bytesToWrite, bar);
   }
@@ -22,8 +33,9 @@ public:
       const std::string& moduleName, const std::string &regName) {
     return (this->get_override("getRegisterAccessor")(moduleName, regName));
   }
-  ~PythonDeviceWrapper() {};
+  virtual ~PythonDeviceWrapper() {};
 };
+
 }
 #endif /* SOURCE_DIRECTORY__DEVBASEWRAPPER_H_ */
 
