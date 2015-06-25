fb6d3a8d0a50faf8ceed309ce4be9a4ca1416022
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-06-25 15:20:43 +0000

    Merge from branch branchForDebPackaging. First version of the packaging framework is going in this commit
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@1143 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ cmake/prepare_debian_package.cmake
@@ -0,0 +1,53 @@
+
+#Prepare the debian control files from the template.
+#Basically this is setting the correct version number in most of the files
+
+
+string(TOLOWER ${PROJECT_NAME} PACKAGE_NAME)
+
+# This is a hack to get escape ${} as the variable expansion in the files copied
+# through configure_file
+set(pD "$")
+
+
+
+#Nothing to change, just copy
+file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/compat
+           ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/rules
+     DESTINATION debian_from_template)
+
+file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/source/format
+     DESTINATION debian_from_template/source)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/control.in
+               debian_from_template/control)
+               
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/pkg.install.in
+               debian_from_template/${PACKAGE_NAME}.install)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/pkg-empty.install.in
+               debian_from_template/${PACKAGE_NAME}-empty.install)
+
+
+
+
+#Set the version number
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/copyright.in
+               debian_from_template/copyright @ONLY)
+
+#Copy and configure the shell script which performs the actual
+#building of the package
+configure_file(${CMAKE_SOURCE_DIR}/cmake/make_debian_package.sh.in
+               make_debian_package.sh)
+               
+#A custom target so you can just run make debian_package
+#(You could instead run make_debian_package.sh yourself, hm...)
+add_custom_target(debian_package ${CMAKE_BINARY_DIR}/make_debian_package.sh
+                  COMMENT Building debian package for tag ${MtcaMappedDevice_VERSION})
+
+#For convenience: Also create an install script for DESY
+set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_NAME}_*.changes")
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/install_debian_package_at_DESY.sh.in
+               install_debian_package_at_DESY.sh @ONLY)
+
