4312b9b4373565733753cc1cbedef28330294190
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-08-12 14:23:11 +0000

    Deleted HelperFunctions.cc and moved code to .h
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/PythonBindings/MtcaMappedDevice/trunk@1296 5f601a5c-88b9-47f3-b851-818f6741d00d

--- HelperFunctions.h
+++ HelperFunctions.h
@@ -1,7 +1,9 @@
 #ifndef HELPERFUNCTIONS_H_
 #define HELPERFUNCTIONS_H_
 #include <boost/python.hpp>
-
+#include "HelperFunctions.h"
+#include <numpy/arrayobject.h>
+#include "PythonExceptions.h"
 namespace bp = boost::python;
 
 namespace mtca4upy {
@@ -14,14 +16,42 @@ enum numpyDataTypes {
   USUPPORTED_TYPE
 };
 
-char *extractDataPointer(const bp::numeric::array &Buffer);
+inline size_t extractNumberOfElements(const bp::numeric::array& dataToWrite) {
+  return (boost::python::extract<long>(dataToWrite.attr("size")));
+}
 
-numpyDataTypes extractDataType(const bp::numeric::array &Buffer);
+inline char* extractDataPointer(const bp::numeric::array& Buffer) {
+  PyArrayObject* numpyArrayObj = reinterpret_cast<PyArrayObject*>(Buffer.ptr());
+  return (numpyArrayObj->data);
+}
 
-void throwExceptionIfOutOfBounds(const bp::numeric::array &dataToWrite,
-                                 const size_t &bytesToWrite);
+inline void throwExceptionIfOutOfBounds(const bp::numeric::array& dataToWrite,
+                                        const size_t& bytesToWrite) {
+  size_t bytesInArray =
+      (extractNumberOfElements(dataToWrite) * sizeof(int32_t));
+  if (bytesInArray < bytesToWrite) {
+    throw mtca4upy::ArrayOutOfBoundException(
+        "size to write is more than the supplied array size");
+  }
+}
 
-size_t extractNumberOfElements(const bp::numeric::array &dataToWrite);
+inline mtca4upy::numpyDataTypes extractDataType(
+    const bp::numeric::array& Buffer) {
+  PyArrayObject* numpyArrayObj = reinterpret_cast<PyArrayObject*>(Buffer.ptr());
+  int type_number = PyArray_TYPE(numpyArrayObj);
+
+  if (type_number == NPY_INT32) {
+    return INT32;
+  } else if (type_number == NPY_INT64) {
+    return INT64;
+  } else if (type_number == NPY_FLOAT32) {
+    return FLOAT32;
+  } else if (type_number == NPY_FLOAT64) {
+    return FLOAT64;
+  } else {
+    return USUPPORTED_TYPE;
+  }
+}
 }
 
 #endif /* HELPERFUNCTIONS_H_ */
