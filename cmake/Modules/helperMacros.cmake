61021e22813d7a8a0ed8f8a11b76ff41681ca6c5
vargheseg <vargheseg@users.noreply.github.com>
2017-04-10 19:45:41 +0200

    Bug Fixes for CMakeLists.txt
    - min_req_mtca4u-deviceaccess_version initialized before use.
    - PYTHONLIBS_VERSION_STRING used for selecting corresponding boost-python
      component and deciding package install path.
    - package installs to site-packages or dist-packages depending on
      CMAKE_INSTALL_PREFIX.

--- cmake/Modules/helperMacros.cmake
+++ cmake/Modules/helperMacros.cmake
@@ -14,7 +14,7 @@ set(PYTHON3 FALSE CACHE BOOL
 # Returns the python interpreter to be used for teh project. This is indicated
 # by the user by invoking cmake with the PYTHON3 bool as True: 
 # cmake -DPYTHON3=TRUE ../ 
-function(get_python_interpreter python_interpreter)
+function(get_python_interpreter_string python_interpreter)
   if(PYTHON3)
       set(${python_interpreter} "python3" PARENT_SCOPE)
   else()
@@ -26,43 +26,74 @@ endfunction()
 
 # Returns version number depending on choice of compilation; 3 for python3 and
 # 2.7 otherwise
-function(get_python_lib_version version_num)
+function(get_desired_python_major_release version_num)
   if(PYTHON3)
     set(${version_num} "3" PARENT_SCOPE)
   else()
-    set(${version_num} "2.7" PARENT_SCOPE)
+    set(${version_num} "2" PARENT_SCOPE)
   endif()
-endfunction()
-
-function(get_boost_python_component_name, component_name)
-endfunction()
-
-
-function(get_boost_python_component_name boost_python)
-    if(PYTHON3)
-        extract_ubuntu_variant(os_name)
-        
-        if(${os_name} STREQUAL "xenial")
-            set(${boost_python} "python-py35" PARENT_SCOPE)
-        elseif(${os_name} STREQUAL "trusty")
-            set(${boost_python} "python-py34" PARENT_SCOPE)
-        else()
-            set(${boost_python} "python3" PARENT_SCOPE)
-        endif()
-            
-    else()
-        set(${boost_python} "python" PARENT_SCOPE)
-    endif()
 endfunction()
 
 # os_code_name, empty if not found
 function(extract_ubuntu_variant os_code_name)
-  find_program(lsb_release lsb_release)
-  execute_process(COMMAND ${lsb_release} -cs
-  OUTPUT_VARIABLE lsb_release_os_codename
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(${os_code_name} ${lsb_release_os_codename} PARENT_SCOPE)
+    find_program(lsb_release lsb_release)
+    execute_process(COMMAND ${lsb_release} -cs
+                    OUTPUT_VARIABLE lsb_release_os_codename
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(${os_code_name} ${lsb_release_os_codename} PARENT_SCOPE)
 endfunction()
+
+#
+# Returns os_vendor as Ubuntu if applicable 
+function (get_os_distro_vendor os_vendor)
+    find_program(lsb_release lsb_release)
+        execute_process(COMMAND ${lsb_release} -is
+                    OUTPUT_VARIABLE lsb_vendor_name
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+                    
+    set(${os_vendor} ${lsb_vendor_name} PARENT_SCOPE)
+endfunction()
+
+
+function(get_python_module_install_path python_version_string install_path)
+    get_os_distro_vendor(os_vendor)
+    convert_version_string_to_list(${python_version_string} version_list)
+    list(GET version_list 0 major_version)
+    list(GET version_list 1 minor_version)
+    
+    if(("${os_vendor}" STREQUAL "Ubuntu") AND 
+    (("${CMAKE_INSTALL_PREFIX}" STREQUAL "/usr") OR
+     ("${CMAKE_INSTALL_PREFIX}" STREQUAL "/usr/local")))
+
+        if(PYTHON3)
+            set(${install_path} 
+                "lib/python${major_version}/dist-packages" PARENT_SCOPE)
+        else()
+            set(${install_path} 
+                "lib/python${major_version}.${minor_version}/dist-packages" 
+                PARENT_SCOPE)
+        endif()
+
+    else()
+        set(${install_path} 
+            "lib/python${major_version}.${minor_version}/site-packages"
+            PARENT_SCOPE)
+    endif()
+    
+endfunction()
+
+function (get_boost_python_string pythonlib_version boost_python)
+    convert_version_string_to_list(${pythonlib_version} version_list)
+    list(GET version_list 0 major_version)
+    list(GET version_list 1 minor_version)
+    set(${boost_python} "python-py${major_version}${minor_version}" PARENT_SCOPE)
+endfunction()
+
+function (convert_version_string_to_list version_string version_list)
+    string(REPLACE "." ";" tmp ${${version_string}})
+    set(${version_list} ${tmp} PARENT_SCOPE)
+endfunction()
+
 
 # the directory parameter takes in a list of directories within the source
 # directory. The macro copies supported file extensions from these specified
