127a6dfce3405b66be31616d139f3a301a045519
vargheseg <vargheseg@users.noreply.github.com>
2018-01-09 13:30:26 +0100

    stubfile: testScalarAccessor.py

---
+++ tests/testScalarAccessor.py
@@ -0,0 +1,13 @@
+
+
+def test_get_scalar_accessor():
+  getScalarRegisterAccessor(device, registerPathName, wordOffsetInRegister, access_mode_flags) const;
+  device.getScalarRegisterAccessor( registerPathName, wordOffsetInRegister, access_mode_flags) const;
+  pass
+
+def test_get_scalar_accessor():
+  pass
+
+def test_get_scalar_accessor():
+  pass
+
