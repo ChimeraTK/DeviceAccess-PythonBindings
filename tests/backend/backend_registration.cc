ffd2b39acfba20fc8b1fdcdaf52e8d3fce4e8922
vargheseg <vargheseg@users.noreply.github.com>
2018-10-18 11:50:10 +0200

    refactoring for backend registration

--- tests/backend/backend_registration.cc
+++ tests/backend/backend_registration.cc
@@ -1,34 +1,4 @@
-#include <ChimeraTK/DeviceBackendImpl.h>
-#include <ChimeraTK/BackendFactory.h>
-#include <ChimeraTK/DeviceAccessVersion.h>
+#include "backend_registration.h"
 
-#include "backend.h"
 
-namespace TestBackend {
-using Backend_t = boost::shared_ptr<ChimeraTK::DeviceBackend>;
-class BackendRegisterer {
-public:
-  BackendRegisterer();
-
-private:
-  static Backend_t createBackend(std::string /*host*/, std::string /*instance*/,
-                                 std::list<std::string> parameters,
-                                 std::string /*mapFileName*/);
-};
-
-BackendRegisterer::BackendRegisterer() {
-  ChimeraTK::BackendFactory::getInstance().registerBackendType(
-      "TestBackend", "", &BackendRegisterer::createBackend,
-      CHIMERATK_DEVICEACCESS_VERSION);
-}
-Backend_t BackendRegisterer::createBackend(std::string /*host*/,     //
-                                           std::string /*instance*/, //
-                                           std::list<std::string>,
-                                           std::string /*mapFileName*/) {
-  return Backend_t(new Backend(TestBackend::getDummyList()));
-}
-
-// Invoke registration on loading module.
-// fixme: protect agaist removal by compiler optimizations
-static auto registerer = BackendRegisterer();
-} // namespace TestBackend
+ TestBackend::BackendRegisterer  registerer = TestBackend::BackendRegisterer();
