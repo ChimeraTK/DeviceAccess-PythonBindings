f0c648e51a502bb08eff8cfa08ebdade660bd276
Automated MSK Jenkins User <msk_jenkins@msk-ubuntu1604.desy.de>
2020-01-28 09:58:17 +0000

    automatic merge of project-template

--- cmake/create_cmake_config_files.cmake
+++ cmake/create_cmake_config_files.cmake
@@ -48,9 +48,17 @@ foreach(LIBRARY ${LIST})
     set(${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE "${${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE} ${LIBRARY}")
   elseif(LIBRARY MATCHES "^-l")   # library name does not contain slashes but already the -l option: directly quote it
     set(${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE "${${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE} ${LIBRARY}")
+  elseif(LIBRARY MATCHES "::")  # library name is an exported target - we need to resolve it for Makefiles
+    get_property(lib_loc TARGET ${LIBRARY} PROPERTY LOCATION)
+    string(APPEND ${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE " ${lib_loc}")
   else()                          # link against library with -l option
     set(${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE "${${PROJECT_NAME}_LINKER_FLAGS_MAKEFILE} -l${LIBRARY}")
   endif()
+endforeach()
+
+set(${PROJECT_NAME}_PUBLIC_DEPENDENCIES_L "")
+foreach(DEPENDENCY ${${PROJECT_NAME}_PUBLIC_DEPENDENCIES})
+    string(APPEND ${PROJECT_NAME}_PUBLIC_DEPENDENCIES_L "find_package(${DEPENDENCY} REQUIRED)\n")
 endforeach()
 
 # we have nested @-statements, so we have to parse twice:
