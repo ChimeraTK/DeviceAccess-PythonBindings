655c5dcdc35f9ad74855bd77cbbb73b2b1cefe28
Christian Willner <34183939+vaeng@users.noreply.github.com>
2023-06-26 17:38:06 +0200

    Fix version number for setAndWrite (#38)
    * Add test to see failing version number
    
    * fix failing version number for setandwrite

--- PythonModuleMethods.h
+++ PythonModuleMethods.h
@@ -167,6 +167,9 @@ namespace mtca4upy {
 
     template<typename T>
     void setAndWrite(ChimeraTK::ScalarRegisterAccessor<T>& self, T newValue, ChimeraTK::VersionNumber versionNumber) {
+      if (versionNumber == ChimeraTK::VersionNumber(nullptr)) {
+        versionNumber = ChimeraTK::VersionNumber();
+      }
       self.setAndWrite(newValue, versionNumber);
     }
 
