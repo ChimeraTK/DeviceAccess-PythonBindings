7a015e2aec62d7fcb6e2b12dba6b8829f1ae2379
Martin Hierholzer <martin.hierholzer@desy.de>
2024-01-18 16:01:47 +0100

    feat: support boolean types

--- tests/testAccessors.py
+++ tests/testAccessors.py
@@ -20,16 +20,32 @@ class TestAccessors(unittest.TestCase):
         da.setDMapFilePath("deviceInformation/exampleCrate.dmap")
         dev = da.Device("CARD_WITH_MODULES")
         dev.open()
+
         acc = dev.getScalarRegisterAccessor(np.int32, "ADC/WORD_CLK_CNT_1")
         reference = [99]
-
         acc.set(reference)
         acc.write()
 
-        otherAcc = dev.getScalarRegisterAccessor(
-            np.int32, "ADC/WORD_CLK_CNT_1")
+        otherAcc = dev.getScalarRegisterAccessor(np.int32, "ADC/WORD_CLK_CNT_1")
         otherAcc.read()
+
         self.assertTrue(reference == otherAcc.view())
+
+        boolAcc = dev.getScalarRegisterAccessor(bool, "ADC/WORD_CLK_CNT_1")
+        boolAcc.read()
+        self.assertTrue(boolAcc)
+
+        reference = 0
+        acc.set(reference)
+        acc.write()
+        boolAcc.read()
+        self.assertFalse(boolAcc)
+
+        boolAcc.set(True)
+        boolAcc.write()
+        acc.read()
+        self.assertTrue(acc == 1)
+
         dev.close()
 
     def testOneD(self):
@@ -37,17 +53,40 @@ class TestAccessors(unittest.TestCase):
         da.setDMapFilePath("deviceInformation/exampleCrate.dmap")
         dev = da.Device("CARD_WITH_MODULES")
         dev.open()
+
         acc = dev.getOneDRegisterAccessor(np.int32, "BOARD/WORD_CLK_MUX")
         elements = acc.getNElements()
-        reference = [i + 42 for i in range(elements)]
+        self.assertTrue(elements == 4)
 
+        reference = [i + 42 for i in range(elements)]
         acc.set(reference)
         acc.write()
 
-        otherAcc = dev.getOneDRegisterAccessor(
-            np.int32, "BOARD/WORD_CLK_MUX")
+        otherAcc = dev.getOneDRegisterAccessor(np.int32, "BOARD/WORD_CLK_MUX")
         otherAcc.read()
         self.assertTrue(np.array_equal(reference, otherAcc.view()))
+
+        boolAcc = dev.getOneDRegisterAccessor(bool, "BOARD/WORD_CLK_MUX")
+        referenceBool = [True]*elements
+        boolAcc.read()
+        self.assertTrue(np.array_equal(referenceBool, boolAcc.view()))
+
+        reference = [0, 1, 0, 1]
+        referenceBool = [False, True, False, True]
+        acc.set(reference)
+        acc.write()
+
+        boolAcc.read()
+        self.assertTrue(np.array_equal(referenceBool, boolAcc.view()))
+
+        referenceBool = [False, False, True, True]
+        reference = [0, 0, 1, 1]
+        boolAcc.set(referenceBool)
+        boolAcc.write()
+
+        acc.read()
+        self.assertTrue(np.array_equal(reference, acc.view()))
+
         dev.close()
 
     def testTwoD(self):
@@ -66,7 +105,33 @@ class TestAccessors(unittest.TestCase):
 
         otherAcc = dev.getTwoDRegisterAccessor(np.int32, "BOARD/DMA")
         otherAcc.read()
-        self.assertTrue(np.array_equal(reference, otherAcc.view()))
+        self.assertTrue(np.array_equal(reference, otherAcc))
+
+        boolAcc = dev.getTwoDRegisterAccessor(bool, "BOARD/DMA")
+        referenceBool = [[True]*elementsPerChannel]*channels
+        boolAcc.read()
+        self.assertTrue(np.array_equal(referenceBool, boolAcc))
+
+        reference = [
+            [1 if i == j else 0 for j in range(elementsPerChannel)] for i in range(channels)]
+        referenceBool = [
+            [i == j for j in range(elementsPerChannel)] for i in range(channels)]
+        acc.set(reference)
+        acc.write()
+
+        boolAcc.read()
+        self.assertTrue(np.array_equal(referenceBool, boolAcc))
+
+        reference = [
+            [1 if i < j else 0 for j in range(elementsPerChannel)] for i in range(channels)]
+        referenceBool = [
+            [i < j for j in range(elementsPerChannel)] for i in range(channels)]
+        boolAcc.set(referenceBool)
+        boolAcc.write()
+
+        acc.read()
+        self.assertTrue(np.array_equal(reference, acc))
+
         dev.close()
 
     def testTypes(self):
