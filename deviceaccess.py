7ea2a01a95036e079130b0325e02edf0c8ac4a11
Martin Hierholzer <martin.hierholzer@desy.de>
2024-01-29 15:14:14 +0100

    fix: add missing spdx file headers

--- deviceaccess.py
+++ deviceaccess.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+# SPDX-License-Identifier: LGPL-3.0-or-later
 """
 This module offers the functionality of the DeviceAccess C++ library for python.
 
--- include/PythonExceptions.h
+++ include/PythonExceptions.h
@@ -1,5 +1,6 @@
-#ifndef SOURCE_DIRECTORY__PYTHONEXCEPTION_H_
-#define SOURCE_DIRECTORY__PYTHONEXCEPTION_H_
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
 
 #include <exception>
 #include <string>
