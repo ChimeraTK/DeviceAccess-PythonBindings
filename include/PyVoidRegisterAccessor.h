bdf89182e3d273fc687b153c0505b0a44a7f170f
vaeng <34183939+vaeng@users.noreply.github.com>
2025-04-15 18:15:08 +0200

    fixup! feat: change to pybind11

---
+++ include/PyVoidRegisterAccessor.h
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#pragma once
+
+#include <pybind11/pybind11.h>
+// pybind11.h must come first
+
+#include "PyTransferElement.h"
+
+#include <ChimeraTK/AccessMode.h>
+#include <ChimeraTK/SupportedUserTypes.h>
+#include <ChimeraTK/VariantUserTypes.h>
+#include <ChimeraTK/VoidRegisterAccessor.h>
+
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+
+namespace ChimeraTK {
+
+  /********************************************************************************************************************/
+
+  class PyVoidRegisterAccessor : public VoidRegisterAccessor {
+   public:
+    using VoidRegisterAccessor::VoidRegisterAccessor;
+
+    std::string repr(py::object& acc) const;
+
+    static void bind(py::module& mod);
+  };
+
+  /********************************************************************************************************************/
+
+} // namespace ChimeraTK
