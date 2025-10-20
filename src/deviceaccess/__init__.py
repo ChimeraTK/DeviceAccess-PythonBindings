a1c8a1222ed7dbceca4996bdaf605f377d44ba75
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-10-20 16:06:16 +0200

    fix: improved modules

--- src/deviceaccess/__init__.py
+++ src/deviceaccess/__init__.py
@@ -5,4 +5,5 @@ try:
 except Exception:
     __all__ = tuple(n for n in dir(deviceaccess) if not n.startswith("_"))
 
+# Populate the namespace eagerly
 globals().update({name: getattr(deviceaccess, name) for name in __all__})
