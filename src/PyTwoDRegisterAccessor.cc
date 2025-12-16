0a0fb1db5296fa1077b2ac945206f9c40126d9cc
Christian Willner <34183939+vaeng@users.noreply.github.com>
2025-12-16 16:26:56 +0100

    docs: add sphinx-style docstrings

--- src/PyTwoDRegisterAccessor.cc
+++ src/PyTwoDRegisterAccessor.cc
@@ -210,7 +210,7 @@ namespace ChimeraTK {
   /********************************************************************************************************************/
 
   std::string PyTwoDRegisterAccessor::repr(py::object& acc) const {
-    std::string rep{"<TwoDRegisterAccessor(type="};
+    std::string rep{"<PyTwoDRegisterAccessor(type="};
     rep.append(py::cast<py::object>(py::cast(&acc).attr("getValueType")()).attr("__repr__")().cast<std::string>());
     rep.append(", name=");
     rep.append(py::cast(&acc).attr("getName")().cast<std::string>());
@@ -296,84 +296,206 @@ namespace ChimeraTK {
   /********************************************************************************************************************/
 
   void PyTwoDRegisterAccessor::bind(py::module& m) {
-    py::class_<PyTwoDRegisterAccessor, PyTransferElementBase> arrayacc(
-        m, "TwoDRegisterAccessor", py::buffer_protocol());
+    py::class_<PyTwoDRegisterAccessor, PyTransferElementBase> arrayacc(m, "TwoDRegisterAccessor",
+        R"(Accessor class to read and write 2D registers transparently by using the accessor like a two-dimensional array.
+
+      The accessor exposes channels and per-channel elements. Conversion to and from the UserType is handled by a
+      data converter matching the register description (if applicable).
+
+      Note:
+        Create instances via Device.getTwoDRegisterAccessor(). Transfers between the device and the internal buffer
+        need to be triggered using read() and write() before reading from or after writing to the buffer.)",
+        py::buffer_protocol());
     arrayacc.def(py::init<>())
         .def_buffer(&PyTwoDRegisterAccessor::getBufferInfo)
         .def("read", &PyTwoDRegisterAccessor::read,
-            "Read the data from the device.\n\nIf AccessMode::wait_for_new_data was set, this function will block "
-            "until new data has arrived. Otherwise it still might block for a short time until the data transfer was "
-            "complete.")
+            R"(Read the data from the device.
+
+        If AccessMode.wait_for_new_data was set, this function will block until new data has arrived. Otherwise it
+        still might block for a short time until the data transfer was complete.)")
         .def("readNonBlocking", &PyTwoDRegisterAccessor::readNonBlocking,
-            "Read the next value, if available in the input buffer.\n\nIf AccessMode::wait_for_new_data was set, "
-            "this "
-            "function returns immediately and the return value indicated if a new value was available (true) or not "
-            "(false).\n\nIf AccessMode::wait_for_new_data was not set, this function is identical to read(), which "
-            "will still return quickly. Depending on the actual transfer implementation, the backend might need to "
-            "transfer data to obtain the current value before returning. Also this function is not guaranteed to be "
-            "lock free. The return value will be always true in this mode.")
+            R"(Read the next value, if available in the input buffer.
+
+        If AccessMode.wait_for_new_data was set, this function returns immediately and the return value indicates
+        if a new value was available (true) or not (false).
+
+        If AccessMode.wait_for_new_data was not set, this function is identical to read(), which will still return
+        quickly. Depending on the actual transfer implementation, the backend might need to transfer data to obtain
+        the current value before returning. Also this function is not guaranteed to be lock free. The return value
+        will be always true in this mode.
+
+        :return: True if new data was available, false otherwise.
+        :rtype: bool)")
         .def("readLatest", &PyTwoDRegisterAccessor::readLatest,
-            "Read the latest value, discarding any other update since the last read if present.\n\nOtherwise this "
-            "function is identical to readNonBlocking(), i.e. it will never wait for new values and it will return "
-            "whether a new value was available if AccessMode::wait_for_new_data is set.")
+            R"(Read the latest value, discarding any other update since the last read if present.
+
+        Otherwise this function is identical to readNonBlocking(), i.e. it will never wait for new values and it
+        will return whether a new value was available if AccessMode.wait_for_new_data is set.
+
+        :return: True if new data was available, false otherwise.
+        :rtype: bool)")
         .def("write", &PyTwoDRegisterAccessor::write,
-            "Write the data to device.\n\nThe return value is true, old data was lost on the write transfer (e.g. "
-            "due "
-            "to an buffer overflow). In case of an unbuffered write transfer, the return value will always be false.",
+            R"(Write the buffered data to the device.
+
+        The return value is true if old data was lost on the write transfer (e.g. due to a buffer overflow).
+        In case of an unbuffered write transfer, the return value will always be false.
+
+        :param versionNumber: Version number to use for this write operation. If not specified, a new version
+          number is generated.
+        :type versionNumber: VersionNumber)",
             py::arg("versionNumber") = PyVersionNumber::getNullVersion())
         .def("writeDestructively", &PyTwoDRegisterAccessor::writeDestructively,
-            "Just like write(), but allows the implementation to destroy the content of the user buffer in the "
-            "process.\n\nThis is an optional optimisation, hence there is a default implementation which just calls "
-            "the normal doWriteTransfer(). In any case, the application must expect the user buffer of the "
-            "TransferElement to contain undefined data after calling this function.",
+            R"(Just like write(), but allows the implementation to destroy the content of the user buffer in the
+        process.
+
+        This is an optional optimisation, hence there is a default implementation which just calls write(). In any
+        case, the application must expect the user buffer of the accessor to contain undefined data after calling
+        this function.
+
+        :param versionNumber: Version number to use for this write operation. If not specified, a new version
+          number is generated.
+        :type versionNumber: VersionNumber)",
             py::arg("versionNumber") = PyVersionNumber::getNullVersion())
         .def("interrupt", &PyTwoDRegisterAccessor::interrupt,
-            "Return from a blocking read immediately and throw the ThreadInterrupted exception.")
-        .def("getName", &PyTwoDRegisterAccessor::getName, "Returns the name that identifies the process variable.")
+            R"(Interrupt a blocking read operation.
+
+        This will cause a blocking read to return immediately and throw an InterruptedException.)")
+        .def("getName", &PyTwoDRegisterAccessor::getName,
+            R"(Returns the name that identifies the process variable.
+
+        :return: The register name.
+        :rtype: str)")
         .def("getUnit", &PyTwoDRegisterAccessor::getUnit,
-            "Returns the engineering unit.\n\nIf none was specified, it will default to ' n./ a.'")
+            R"(Returns the engineering unit.
+
+        If none was specified, it will default to 'n./a.'.
+
+        :return: The engineering unit string.
+        :rtype: str)")
         .def("getDescription", &PyTwoDRegisterAccessor::getDescription,
-            "Returns the description of this variable/register.")
+            R"(Returns the description of this variable/register.
+
+        :return: The description string.
+        :rtype: str)")
         .def("getValueType", &PyTwoDRegisterAccessor::getValueType,
-            "Returns the std::type_info for the value type of this transfer element.\n\nThis can be used to "
-            "determine "
-            "the type at runtime.")
+            R"(Returns the type_info for the value type of this accessor.
+
+        This can be used to determine the type at runtime.
+
+        :return: Type information object.
+        :rtype: type)")
         .def("getVersionNumber", &PyTwoDRegisterAccessor::getVersionNumber,
-            "Returns the version number that is associated with the last transfer (i.e. last read or write)")
+            R"(Returns the version number that is associated with the last transfer.
+
+        This refers to the last read or write operation.
+
+        :return: The version number of the last transfer.
+        :rtype: VersionNumber)")
         .def("isReadOnly", &PyTwoDRegisterAccessor::isReadOnly,
-            "Check if transfer element is read only, i.e. it is readable but not writeable.")
-        .def("isReadable", &PyTwoDRegisterAccessor::isReadable, "Check if transfer element is readable.")
-        .def("isWriteable", &PyTwoDRegisterAccessor::isWriteable, "Check if transfer element is writeable.")
+            R"(Check if accessor is read only.
+
+        This means it is readable but not writeable.
+
+        :return: True if read only, false otherwise.
+        :rtype: bool)")
+        .def("isReadable", &PyTwoDRegisterAccessor::isReadable,
+            R"(Check if accessor is readable.
+
+        :return: True if readable, false otherwise.
+        :rtype: bool)")
+        .def("isWriteable", &PyTwoDRegisterAccessor::isWriteable,
+            R"(Check if accessor is writeable.
+
+        :return: True if writeable, false otherwise.
+        :rtype: bool)")
         .def("getId", &PyTwoDRegisterAccessor::getId,
-            "Obtain unique ID for the actual implementation of this TransferElement.\n\nThis means that e.g. two "
-            "instances of ScalarRegisterAccessor created by the same call to Device::getScalarRegisterAccessor() "
-            "(e.g. "
-            "by copying the accessor to another using NDRegisterAccessorBridge::replace()) will have the same ID, "
-            "while two instances obtained by to difference calls to Device::getScalarRegisterAccessor() will have a "
-            "different ID even when accessing the very same register.")
+            R"(Obtain unique ID for the actual implementation of this accessor.
+
+        This means that e.g. two instances of TwoDRegisterAccessor created by the same call to
+        Device.getTwoDRegisterAccessor() will have the same ID, while two instances obtained by two different
+        calls to Device.getTwoDRegisterAccessor() will have a different ID even when accessing the very same
+        register.
+
+        :return: The unique transfer element ID.
+        :rtype: TransferElementID)")
         .def("dataValidity", &PyTwoDRegisterAccessor::dataValidity,
-            "Return current validity of the data.\n\nWill always return DataValidity.ok if the backend does not "
-            "support it")
-        .def("isInitialised", &PyTwoDRegisterAccessor::isInitialised, "Check if the transfer element is initialised.")
+            R"(Return current validity of the data.
+
+        Will always return DataValidity.ok if the backend does not support it.
+
+        :return: The current data validity state.
+        :rtype: DataValidity)")
+        .def("isInitialised", &PyTwoDRegisterAccessor::isInitialised,
+            R"(Check if the accessor is initialised.
+
+        :return: True if initialised, false otherwise.
+        :rtype: bool)")
         .def("getNElementsPerChannel", &PyTwoDRegisterAccessor::getNElementsPerChannel,
-            "Return number of elements/samples per Channel in the register.")
-        .def("getNChannels", &PyTwoDRegisterAccessor::getNChannels, "Return number of Channels in the register.")
-        .def("get", &PyTwoDRegisterAccessor::get, "Return an array of UserType (without a previous read).")
-        .def("set", &PyTwoDRegisterAccessor::set, "Set the values of the array of UserType.", py::arg("newValue"))
+            R"(Return number of elements/samples per channel in the register.
+
+        :return: Number of elements per channel.
+        :rtype: int)")
+        .def("getNChannels", &PyTwoDRegisterAccessor::getNChannels,
+            R"(Return number of channels in the register.
+
+        :return: Number of channels.
+        :rtype: int)")
+        .def("get", &PyTwoDRegisterAccessor::get,
+            R"(Return a 2D array of UserType from the internal buffer (without a previous read).
+
+        The returned object is typically a numpy ndarray with shape (channels, elements_per_channel). For string
+        registers, a list of lists is returned instead.
+
+        :return: Current buffer content as a 2D array-like object.
+        :rtype: ndarray or list)")
+        .def("set", &PyTwoDRegisterAccessor::set, py::arg("newValue"),
+            R"(Set the values of the 2D array buffer.
+
+        :param newValue: New values to set, shaped as [channels][elements] or a 2D numpy array.
+        :type newValue: list[list[UserType]] or ndarray)")
         .def("getAsCooked", &PyTwoDRegisterAccessor::getAsCooked,
-            "Get the cooked values in case the accessor is a raw accessor (which does not do data conversion). This "
-            "returns the converted data from the use buffer. It does not do any read or write transfer.",
+            R"(Get a cooked value for a specific channel and element when the accessor is raw (no data conversion).
+
+        This returns the converted data from the user buffer. It does not do any read or write transfer.
+
+        :param channel: Channel index.
+        :type channel: int
+        :param element: Element index within the channel.
+        :type element: int
+        :return: The cooked value.
+        :rtype: float)",
             py::arg("channel"), py::arg("element"))
         .def("setAsCooked", &PyTwoDRegisterAccessor::setAsCooked,
-            "Set the cooked values in case the accessor is a raw accessor (which does not do data conversion). This "
-            "converts to raw and writes the data to the user buffer. It does not do any read or write transfer.",
+            R"(Set a cooked value for a specific channel and element when the accessor is raw (no data conversion).
+
+        This converts to raw and writes the data to the user buffer. It does not do any read or write transfer.
+
+        :param channel: Channel index.
+        :type channel: int
+        :param element: Element index within the channel.
+        :type element: int
+        :param value: The cooked value to set.
+        :type value: float)",
             py::arg("channel"), py::arg("element"), py::arg("value"))
         .def("setDataValidity", &PyTwoDRegisterAccessor::setDataValidity,
-            "Set the data validity of the transfer element.")
+            R"(Set the data validity of the accessor.
+
+        :param validity: The data validity state to set.
+        :type validity: DataValidity)")
         .def("getAccessModeFlags", &PyTwoDRegisterAccessor::getAccessModeFlags,
-            "Return the access mode flags that were used to create this TransferElement.\n\nThis can be used to "
-            "determine the setting of the `raw` and the `wait_for_new_data` flags")
-        .def("__getitem__", &PyTwoDRegisterAccessor::getitem, "Get an element from the array by index.")
+            R"(Return the access mode flags that were used to create this accessor.
+
+        This can be used to determine the setting of the raw and the wait_for_new_data flags.
+
+        :return: List of access mode flags.
+        :rtype: list[AccessMode])")
+        .def("__getitem__", &PyTwoDRegisterAccessor::getitem, py::arg("index"),
+            R"(Get a single channel by index.
+
+        :param index: Channel index.
+        :type index: int
+        :return: View of the selected channel as a 1D array-like object.
+        :rtype: ndarray or list)")
         .def("__getattr__", &PyTwoDRegisterAccessor::getattr);
 
     for(const auto& fn : PyTransferElementBase::specialFunctionsToEmulateNumeric) {
